9f487b8b04d7165c9ca2315ef541743c900e3c28
Donald Forbes <donald.forbes@temporal.io>
2025-10-28 16:06:40 +0000

    First attempts at using the worker controller.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,11 +4,27 @@
 #
 #  *** Subroutines
 validate() {
-	echo "Validation - NoOp"
+	if mvn --version > /dev/null; then
+	  echo "Maven installed"
+	else 
+	  echo "Please ensure you have Maven installed and available on the command line."
+	  exit 1
+        fi
+	
+	if ytt --version > /dev/null; then
+          echo "ytt installed."
+	else
+	  echo "YTT is not installed please install and make availabe.  (Mac - brew install ytt)"
+	  exit 1
+        fi
 }
 setup_context() {
+	# Get from git the most recent version of files
+        git pull
 	kubectl config set-context --current --namespace=${K8S_NAMESPACE}
 	SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE[0]}")")
+	export WORKER_VERSION_image__version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout -f ${SCRIPT_DIR}/../java/worker-versioning/pom.xml)
+	export WORKER_VERSION_image__url="ghcr.io/${GITHUB_ACCOUNT}/temporal-worker-versioning-demo:${WORKER_VERSION_image__version}"
 }
 create_secrets() {
 	SECRET_NAME=temporal-auth-tls
@@ -17,13 +33,16 @@ create_secrets() {
 }
 #
 source setup-env.sh
-# 
+validate
 setup_context
-create_secrets
 
-env | grep K8S
-echo ${SCRIPT_DIR}
-kubectl apply -f ${SCRIPT_DIR}/../k8s
+#create_secrets
+
+env | grep WORKER_VERSION
+ytt -f ${SCRIPT_DIR}/../k8s/deployment.yaml  -f ${SCRIPT_DIR}/../k8s/worker-versioning-values.yaml --data-values-env WORKER_VERSION | kubectl apply -f -
+kubectl apply -f ${SCRIPT_DIR}/../k8s/temporal-connection.yaml
+kubectl apply -f ${SCRIPT_DIR}/../k8s/ingress-route.yaml
+kubectl apply -f ${SCRIPT_DIR}/../k8s/service.yaml
 kubectl get pods
 kubectl get secrets 
 
--- scripts/setup-env.sh
+++ scripts/setup-env.sh
@@ -2,5 +2,6 @@
 #
 export APP_NAME=temporal-worker-versioning-demo
 export K8S_NAMESPACE=temporal-worker-versioning
+export GITHUB_ACCOUNT=donforbes
 export AUTH_CERT_PATH=$HOME/stuff/source/certificates/temporal-client-leaf.pem
 export AUTH_KEY_PATH=$HOME/stuff/source/certificates/temporal-client.key
