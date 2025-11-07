b7bde8532f1fc48776eb478a2f7360ba7eee95e2
Donald Forbes <donald.forbes@temporal.io>
2025-11-07 20:59:09 +0000

    version 0.0.20

---
+++ java/worker-versioning/src/main/java/io/temporal/demos/worker_versioning/util/GlobalCORSConfiguration.java
@@ -0,0 +1,20 @@
+package io.temporal.demos.worker_versioning.util;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class GlobalCORSConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allow all paths
+                .allowedOrigins("*") // Whitelist these origins
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // The allowed HTTP methods
+                .allowedHeaders("*")
+                .allowCredentials(true) // Allow credentials
+                .maxAge(3600); // Cache preflight response for 1 hour
+    }
+}
+
