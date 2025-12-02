1f8438db55ac1a9133b71e7c3a27c9051840f9a6
Donald Forbes <donald.forbes@temporal.io>
2025-12-02 14:54:49 +0000

    update to try and build file with buildpacks and correct default url for backend

--- frontend/worker-versioning-ui/vite.config.ts
+++ frontend/worker-versioning-ui/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const envFromFiles = loadEnv(mode, process.cwd(), 'VITE_')
-  const apiBaseUrl = process.env.VITE_API_BASE_URL || envFromFiles.VITE_API_BASE_URL || 'http://worker-versioning-api.tmprl-demo.cloud'
+  const apiBaseUrl = process.env.VITE_API_BASE_URL || envFromFiles.VITE_API_BASE_URL || 'http://temporal-worker-versioning-service:80' //  Default to use the service on the assumption the backend is running in this cluster.
 
   return {
     server: { 
